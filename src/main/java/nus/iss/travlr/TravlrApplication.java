7b2615583e3c45f51a921e43ff54b90d6a95b2f6
Jaspereap <jaspereap@gmail.com>
2023-12-19 14:45:52 +0800

    Initial

---
+++ src/main/java/nus/iss/travlr/TravlrApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.travlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravlrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/travlr/TravlrApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.travlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravlrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
